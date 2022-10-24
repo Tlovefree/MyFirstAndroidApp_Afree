f1bd180bfbedd3b621c27ce4dacad71ae688a4e1
lovefree <kmkd87@gmail.com>
2022-10-24 20:24:23 +0900

    Initial commit

---
+++ app/src/main/java/com/keyflow/myfirstandroidapp_afree/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.keyflow.myfirstandroidapp_afree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
